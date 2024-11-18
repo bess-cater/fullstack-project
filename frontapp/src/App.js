7a2a2b914f67c2826e1535c1b7280ceccd42d45f
besscater <caterlilu@gmail.com>
2024-11-18 17:37:22 +0900

    add youtube search API

--- frontapp/src/App.js
+++ frontapp/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [desc, setDesc] = useState([]);
+
+  const fetchData = () => {
+    const API_KEY = 'AIzaSyBvmvBf9scW6TAEMaBjccoHMIEhCs7DgiA';
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${search}&key=${API_KEY}`)
+    .then(response => response.json())
+    .then(desc => {
+      console.log(desc);
+      if (desc.items) {
+        setDesc(desc.items);  // Set the state with the items array
+      } else {
+        console.log("No items found in the response.");
+      }
+    })
+    .catch(err => console.error(err))
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          It's me, Liza!!!!!!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={search} 
+        onChange={e => setSearch(e.target.value)} />
+      <button onClick={fetchData}>Fetch</button>
+      <ul className="list">
+          {  desc.map(repo => 
+              <li className={`video`} key={repo.id.videoId}>
+                <img src={repo.snippet.thumbnails.high.url} alt="Weather icon" />
+                <div className={`desc`}>  {repo.snippet.title}</div>
+              </li>  
+            )
+          }
+      </ul>
+
     </div>
   );
 }
